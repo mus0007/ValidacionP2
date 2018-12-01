5b78a66fa00021172dcbe3d2e4c9dffdaa7531de
Mario Ubierna <mus0007@alu.ubu.es>
2018-12-01 21:08:57 +0100

    Teses Unitarios Corregidos

--- LibClases.test/TestDataBase.cs
+++ LibClases.test/TestDataBase.cs
@@ -19,7 +19,7 @@ namespace LibClases.test
 
             DataBase db = new DataBase();
             //Prueba getUsuarios
-            Assert.AreEqual(db.getUsuarios().Count, 3);
+            Assert.AreEqual(db.getUsuarios().Count, 10);
 
             //Prueba getUsuario
             Assert.AreEqual(db.getUsuario("Usuario1@gmail.com").IdUsuario, 1);
@@ -32,30 +32,31 @@ namespace LibClases.test
             Assert.AreEqual(db.getUsuario("Usuario3@gmail.com").Cuenta, "Usuario3@gmail.com");
             Assert.AreEqual(db.getUsuario("Usuario3@gmail.com").comprobarPass("usuario3"), true);
 
+            
             //Prueba addEncuestas y getEncuestas
-            Assert.AreEqual(db.getEncuestas().Count, 2);
+            Assert.AreEqual(db.getEncuestas().Count, 10);
             Assert.AreEqual(db.addEncuesta("EncuestaPrueba", "Prueba"), true);
             Assert.AreEqual(db.getEncuesta("EncuestaPrueba").Nombre, "EncuestaPrueba");
-            Assert.AreEqual(db.getEncuestas().Count, 3);
-
+            Assert.AreEqual(db.getEncuestas().Count, 11);
+            
             //Prueba getActivas y getNoActivas
-            Assert.AreEqual(db.getActivas().Count, 2);
-            Assert.AreEqual(db.getNoActivas().Count, 1);
-
+            Assert.AreEqual(db.getActivas().Count, 5);
+            Assert.AreEqual(db.getNoActivas().Count, 6);
+            
             //Prueba modificarEncuesta
             Assert.AreEqual(db.modificarEncuesta("EncuestaPrueba", "prueba"), true);
             Assert.AreEqual(db.getEncuesta("EncuestaPrueba").Nombre, "EncuestaPrueba");
             Assert.AreEqual(db.getEncuesta("EncuestaPrueba").Descripcion, "prueba");
-            Assert.AreEqual(db.getActivas().Count, 2);
+            Assert.AreEqual(db.getActivas().Count, 5);
 
             //Prueba borrarEncuesta
             Assert.AreEqual(db.addEncuesta("Prueba2", "prueba2"), true);
             Assert.AreEqual(db.getEncuesta("Prueba2").Nombre, "Prueba2");
             Assert.AreEqual(db.borrarEncuesta("Prueba2"), true);
-            Assert.AreEqual(db.getEncuestas().Count, 3);
+            Assert.AreEqual(db.getEncuestas().Count, 11);
             Assert.AreEqual(db.getEncuesta("Encuesta1").Nombre, "Encuesta1");
             Assert.AreEqual(db.borrarEncuesta("Encuesta1"),true);
-            Assert.AreEqual(db.getEncuestas().Count, 2);
+            Assert.AreEqual(db.getEncuestas().Count, 10);
 
             //Prueba sugerirEncuesta
             db.sugerirEncuesta("Nombre3", "Descripcion3");
